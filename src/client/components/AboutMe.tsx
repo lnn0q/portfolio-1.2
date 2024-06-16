8b15a4e2b6ee6d7576233df8cbf13144155ccc61
lnn0q <denysenko.w0@gmail.com>
2024-06-16 13:37:49 +0300

    Added complete list of skills

--- src/client/components/AboutMe.tsx
+++ src/client/components/AboutMe.tsx
@@ -1,6 +1,93 @@
 import "../styles/AboutMe.css";
 
 const AboutMe = () => {
+  const skills = [
+    {
+      name: "HTML",
+      id: 0,
+    },
+    {
+      name: "CSS",
+      id: 1,
+    },
+    {
+      name: "Javascript",
+      id: 2,
+    },
+    {
+      name: "TypeScript",
+      id: 3,
+    },
+    {
+      name: "React",
+      id: 4,
+    },
+    {
+      name: "Node",
+      id: 5,
+    },
+    {
+      name: "Express",
+      id: 6,
+    },
+    {
+      name: "MongoDB",
+      id: 7,
+    },
+    {
+      name: "NextJS",
+      id: 8,
+    },
+    {
+      name: "Three.js",
+      id: 9,
+    },
+    {
+      name: "SASS/SCSS",
+      id: 10,
+    },
+    {
+      name: "Git/Github/Gitlab",
+      id: 11,
+    },
+    {
+      name: "Rest API",
+      id: 12,
+    },
+    {
+      name: "CRUD",
+      id: 13,
+    },
+    {
+      name: "MVC",
+      id: 14,
+    },
+    {
+      name: "BEM",
+      id: 15,
+    },
+    {
+      name: "FP/OOP",
+      id: 16,
+    },
+    {
+      name: "Docker",
+      id: 17,
+    },
+    {
+      name: "Linux",
+      id: 18,
+    },
+    {
+      name: "Bash",
+      id: 19,
+    },
+    {
+      name: "Blender 3D",
+      id: 20,
+    },
+  ];
+
   return (
     <div className="aboutMe__textContainer">
       <div className="aboutMe__textSubContainer">
@@ -23,6 +110,20 @@ const AboutMe = () => {
         <div className="aboutMe__textBody">
           Right now I’m working on solidifying my current knowledge and learning
           TypeScript, Nest and NextJS.
+        </div>
+      </div>
+      <div className="aboutMe__textSubContainer">
+        <div className="aboutMe__header aboutMe__header--left">
+          Complete list of skills:
+        </div>
+        <div className="aboutMe__textBody">
+          <div className="aboutMe__skillsSubContainer">
+            <ul className="aboutMe__skillsList">
+              {skills.map((skill) => (
+                <li key={skill.id}>{skill.name}</li>
+              ))}
+            </ul>
+          </div>
         </div>
       </div>
     </div>
